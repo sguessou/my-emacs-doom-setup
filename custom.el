ce0473a9daaec16fabc9b1152899432683eeba21
Saad Guessous <guessous_saad@hotmail.com>
2022-11-30 21:30:38 +0200

    My emacs doom configs

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(blacken flymake-python-pyflakes move-text multiple-cursors flycheck-mypy python-isort python-black ##))
+ '(safe-local-variable-values
+   '((cider-clojure-cli-global-options . "-A:dev:eastwood")
+     (cider-clojure-cli-global-options . "-A:dev:test:eastwood"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
